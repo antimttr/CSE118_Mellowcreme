0098e18fe0e478afdd2ec50a0ed52fe9c78f2eae
Thomas Liu <tliu@Thomass-MacBook.local>
2017-11-03 16:12:46 -0700

    Actually added the new files this time

---
+++ app/src/main/java/cse118mellowcreme/vistext/ViewActivity.java
@@ -0,0 +1,13 @@
+package cse118mellowcreme.vistext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
