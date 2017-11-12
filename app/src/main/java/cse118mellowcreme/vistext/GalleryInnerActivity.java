cd5189fadccb4c24e76ebfdd0a909efe48c4cc73
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-11-12 00:13:02 -0800

    Changed gallery labels and made on clicks so that the inner galleries showed proper category names

--- app/src/main/java/cse118mellowcreme/vistext/GalleryInnerActivity.java
+++ app/src/main/java/cse118mellowcreme/vistext/GalleryInnerActivity.java
@@ -1,13 +1,21 @@
 package cse118mellowcreme.vistext;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GalleryInnerActivity extends AppCompatActivity {
+
+    private String category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_inner);
+        TextView categoryName = (TextView)findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        category = intent.getStringExtra("CategoryChosen");
+        categoryName.setText(category);
     }
 }
