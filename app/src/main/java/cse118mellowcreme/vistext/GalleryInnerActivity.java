b6fc13e66cafc7536227359ae30103629f5e56f7
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-11-13 00:54:31 -0800

    Added gallery frontend for organization using some sample pictures

--- app/src/main/java/cse118mellowcreme/vistext/GalleryInnerActivity.java
+++ app/src/main/java/cse118mellowcreme/vistext/GalleryInnerActivity.java
@@ -1,11 +1,19 @@
 package cse118mellowcreme.vistext;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.ExifInterface;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 
@@ -59,6 +67,16 @@ public class GalleryInnerActivity extends AppCompatActivity {
         pictures = getPicturesWithContext();
 
         //set the picture files in the gallery
+        GridView gridView = (GridView) findViewById(R.id.gridView2);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(GalleryInnerActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
+
+
---
+++ app/src/main/java/cse118mellowcreme/vistext/ImageAdapter.java
@@ -0,0 +1,66 @@
+package cse118mellowcreme.vistext;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Kristin Agcaoili on 11/12/2017.
+ * Citing Android Official Developer Guide as source
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(470, 510));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(2, 2, 2, 2);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+
+
+
+}
