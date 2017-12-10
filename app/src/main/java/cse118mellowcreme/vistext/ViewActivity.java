43e7e1413067c7887577e53fa96f4e77ed810c90
Kimberly Ly <kimberly.ly95@gmail.com>
2017-12-09 17:46:00 -0800

    Clarifai service added

--- app/src/main/java/cse118mellowcreme/vistext/ViewActivity.java
+++ app/src/main/java/cse118mellowcreme/vistext/ViewActivity.java
@@ -8,15 +8,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.ExifInterface;
-import android.os.AsyncTask;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
@@ -26,13 +22,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
-import android.text.Html;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,13 +36,10 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import com.facebook.AccessToken;
-import com.facebook.login.LoginManager;
 import org.json.JSONArray;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 
